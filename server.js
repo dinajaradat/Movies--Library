8de4eea30ed816858f4c2a12d264aa5f1a5677ff
dinajar <deenajaradat2@gmail.com>
2023-03-06 20:39:53 +0300

    Lab15

--- server.js
+++ server.js
@@ -8,13 +8,12 @@ server.use(cors());
 const pg = require('pg');
 //const path = require('path');
 const getJson = require('./data.json');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.use(express.json());
 
 
 
 //Routes
-
 server.post('/addMovie', addMovieHandler);
 server.get('/getMovies', getMoviesHandler);
 
@@ -52,90 +51,91 @@ function addMovieHandler(req, res) {
     const sql = 'INSERT INTO movies (movieName,comments)  VALUES ($1,$2) RETURNING * ';
     const values = [movie.movieName, movie.comments];
     console.log(movie);
-    client.query(sql,values)
-    .then((data) => {
-        res.send('data was added');
-    })
-    .catch(error => {
-        res.send('error00');
-    });
+    client.query(sql, values)
+        .then((data) => {
+            res.send('data was added');
+        })
+        .catch ((error) => {
+            errorHandler(error, req, res);
+        });
 }
 
 
-function getMoviesHandler(req,res){
+function getMoviesHandler(req, res) {
     const sql = 'SELECT * FROM movies';
     client.query(sql)
-    .then((data) => {
-        res.send(data.rows);
-    })
-    .catch(error => {
-        res.send('error');
+        .then((data) => {
+            res.send(data.rows);
+        })
+    .catch ((error) => {
+        // console.log(error);
+        errorHandler(error, req, res);
     });
 }
 
-function updateMovieHandler(req,res){
+function updateMovieHandler(req, res) {
     const id = req.params.id;
     const movie = req.body;
     console.log(id);
     console.log(req.body);
     const sql = 'UPDATE movies  SET moviename =$1,comments =$2  WHERE id=$3  RETURNING * ';
-    const values = [movie.moviename, movie.comments, id ];
+    const values = [movie.moviename, movie.comments, id];
 
-    client.query(sql,values)
-    .then((data) => {
-        res.send(data.rows);
-    })
-    .catch(error => {
-        res.send('error');
-    });
-
+    client.query(sql, values)
+        .then((data) => {
+            res.send(data.rows);
+        })
+        .catch ((error) => {
+            errorHandler(error, req, res);
+        });
 }
 
-function deleteMovieHandler(req,res){
+function deleteMovieHandler(req, res) {
     const id = req.params.id;
     const sql = `DELETE FROM movies WHERE id = ${id}`;
     client.query(sql)
-    .then((data) => {
-        res.json({});
-    })
-    .catch(error => {
-        res.send('error00');
-    });
+        .then((data) => {
+            res.json({});
+        })
+        .catch ((error) => {
+            errorHandler(error, req, res);
+        });
 }
 
-function getMovieById(req,res){
+function getMovieById(req, res) {
     const id = req.params.id;
     const sql = 'SELECT * FROM movies WHERE id=$1 ';
     const values = [id];
 
-    client.query(sql,values)
-    .then((data) => {
-        res.send(data.rows);
-    })
-    .catch(error => {
-        res.send('error');
-    });
+    client.query(sql, values)
+        .then((data) => {
+            res.send(data.rows);
+        })
+        .catch ((error) => {
+            errorHandler(error, req, res);
+        });
 
 }
 
 
-function handleError(req, res, par3) {
-    return (`{ 
-        "status": 404,
-        "responseText": "page not found error" 
-    }`);
+function errorHandler(error,req, res) {
+    const err = {
+        status:500,
+        message: error
+    }
+    res.status(500).send(err);
 }
 
 
 client.connect()
-.then(()=>{
-    server.listen(PORT, () => {
-        console.log(`running on port ${PORT}`);
+    .then(() => {
+        server.listen(PORT, () => {
+            console.log(`running on port ${PORT}`);
+        });
+    })
+    .catch(error => {
+        res.send('error')
     });
-})
-.catch(error => {
-    res.send('error')
-});
 
 
 
