de84852fa76ac4d1feff95177850f3b00171b84d
dinajar <deenajaradat2@gmail.com>
2023-03-10 05:14:13 +0300

    lab12

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const server = express();
-require('dotenv').config();
 const cors = require('cors');
 server.use(cors());
 const pg = require('pg');
@@ -10,28 +9,36 @@ const pg = require('pg');
 const getJson = require('./data.json');
 const PORT = process.env.PORT || 3000;
 server.use(express.json());
+const axios = require('axios');
+require('dotenv').config();
 
 
 
 //Routes
-server.post('/addMovie', addMovieHandler);
-server.get('/getMovies', getMoviesHandler);
-
-const conData1 = new conData(getJson.title, getJson.poster_path, getJson.overview);
-
 server.get('/', (req, res) => {
     res.send(conData1);
 })
-
 
 server.get('/favorite', (req, res) => {
     console.log("Welcome to Favorite Page");
 })
 
+server.get('/trending', trendingHandler);
+server.get('/search', searchHandler);
+
+server.post('/addMovie', addMovieHandler);
+server.get('/getMovies', getMoviesHandler);
+
+const conData1 = new conData(getJson.title, getJson.poster_path, getJson.overview);
+
 server.put('/update/:id', updateMovieHandler)
 server.delete('/delete/:id', deleteMovieHandler)
 server.get('/getMovie/:id', getMovieById)
 const client = new pg.Client(process.env.DatabaseURL);
+
+server.get('*',(req,res)=>{
+    res.status(404).send("sorry, somthing went wrong");
+})
 
 
 
@@ -42,6 +49,57 @@ function conData(title, poster_path, overview) {
     this.poster_path = poster_path;
     this.overview = overview;
     return (this.title);
+}
+
+
+function Movie(id, title, release_date, poster_path, overview) {
+    this.id = id;
+    this.title = title;
+    this.release_date = release_date;
+    this.poster_path = poster_path;
+    this.overview = overview;
+}
+
+
+function trendingHandler(req, res) {
+    try {
+        const APIKey = process.env.APIKey;
+        const url = 'https://api.themoviedb.org/3/trending/all/week?api_key=${APIKey}';
+        axios.get(url)
+            .then((result) => {
+                let movResult = result.data.results.map((item) => {
+                    let newMovie = new Movie(item.id, item.title, item.release_date, item.poster_path, item.overview);
+                    return newMovie;
+                })
+                res.send(movResult);
+            })
+            .catch((err) => {
+                console.log("sorry", err);
+                res.status(500).send(err);
+            })
+    }
+    catch (error) {
+        errorHandler(error, req, res);
+    }
+
+}
+
+function searchHandler(req,res){
+    try{
+        const APIKey = process.env.APIKey;
+        const url = 'https://api.themoviedb.org/3/trending/all/week?api_key=${APIKey}&query=from';
+        axios.get(url)
+            .then((result)=>{
+                res.send(result.data.results);
+            })
+            .catch((err)=>{
+                console.log("sorry", err);
+                res.status(500).send(err);
+            })
+    }
+    catch(error){
+        errorHandler(error);
+    }
 }
 
 
@@ -118,11 +176,12 @@ function getMovieById(req,res){
 }
 
 
-function handleError(req, res, par3) {
-    return (`{ 
-        "status": 404,
-        "responseText": "page not found error" 
-    }`);
+function errorHandler(error, req, res) {
+    const err = {
+        status : 500,
+        message : error
+    }
+    res.send(err);
 }
 
 
